1966140fdaffbdce2e9293ddfe2d3737323c2c8c
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-11-27 01:45:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,9 +56,9 @@ visionias_quality_dict = {
 
 visionias_url_extract_pattern = r"(https://.*?playlist.m3u8.*?)\""
 
- bot = Client
+ bot = Client(
      "bot",
-           api_id=api_id,
+      api_id=api_id,
       api_hash=api_hash, 
       bot_token=bot_token)
 
